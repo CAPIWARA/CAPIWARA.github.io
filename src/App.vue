83253943b62332aa1175ba172b52fe7e17eb939b
Vitor Cavalcanti <vitorluizc@outlook.com>
2017-12-18 23:59:55 -0200

    Add App + reset CSS stuff

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,48 @@
 <template>
-	<h1>CAPIWARA</h1>
+	<main class="main-container">
+		<header>
+			<figure>
+				<img src="~./" alt="">
+			</figure>
+		</header>
+		<section class="section-container -">
+			<article class="box">
+				<h2>Sobre o CAPIWARA</h2>
+				<p>Nascemos e vivemos pela paixão de solucionar problemas. Confiamos que em cada dificuldade, há uma possibilidade de construir um ponto inovador e garantístico.</p>
+				<p>O time Capiwara visa oferecer soluções estratégicas e personalizadas para empresas e escritórios, por meio de tecnologias disruptivas e operações outbounding.</p>
+			</article>
+		</section>
+	</main>
 </template>
+
+<script>
+	import SectionContainer from './components/SectionContainer'
+
+	export default {
+		components: { SectionContainer }
+	}
+</script>
+
+<style src="./style/reset.css"></style>
+
+<style>
+	:root {
+		--section-padding: 1rem;
+	}
+
+	@media screen and (min-width: 768px) {
+		:root {
+			--section-padding: 2rem;
+		}
+	}
+
+	.section-container {
+		box-sizing: border-box;
+		padding: var(--section-padding);
+	}
+
+	.section-container > .title {
+		margin: 0;
+	}
+</style>
+
